e6d2857cc0114f56359c0bb4bfb842eed1a36890
front-chan <chan.jor.zz@gmail.com>
2023-05-04 17:08:49 +0900

    page layout(react basic)

--- app/page.js
+++ app/page.js
@@ -1,3 +1,10 @@
 export default function Home() {
-    return <div></div>;
+    let name = "chan";
+
+    return (
+        <div>
+            <h4 className="title">chan fresh</h4>
+            <p className="title-sub">by dev {name}</p>
+        </div>
+    );
 }
