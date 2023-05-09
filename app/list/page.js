746b72343cbf50232eb17b848a30abb348821a98
front-chan <chan.jor.zz@gmail.com>
2023-05-09 15:23:33 +0900

    array, object state study

--- app/list/page.js
+++ app/list/page.js
@@ -4,32 +4,40 @@ import { useState } from "react";
 
 export default function List() {
     let products = ["Tomatoes", "Pasta", "Coconut"];
-    // let count = 0;
-    let [count, setCount] = useState(0);
+    let [count, setCount] = useState([0, 0, 0]);
 
     return (
         <div>
             <h4 className="title">상품목록</h4>
+
+            {/* <span>{count[0]}</span>
+            <button
+                onClick={() => {
+                    let newCount = [...count];
+                    newCount[0]++;
+                    setCount(newCount);
+                }}
+            >
+                +
+            </button>
+            <span>{count[1]}</span>
+            <button>+</button>
+            <span>{count[2]}</span>
+            <button>+</button> */}
+
             {products.map((p, i) => (
                 <div className="food" key={i}>
                     <img src={`/food${i}.png`} alt={p} className="food-img" />
                     <h4>{p} $40</h4>
-                    <span> {count} </span>
+                    <span> {count[i]} </span>
                     <button
                         onClick={() => {
-                            // console.log(1);
-                            setCount(count + 1);
+                            let newCount = [...count];
+                            newCount[i]++;
+                            setCount(newCount);
                         }}
                     >
                         +
-                    </button>
-                    <button
-                        onClick={() => {
-                            // console.log(1);
-                            setCount(count - 1);
-                        }}
-                    >
-                        -
                     </button>
                 </div>
             ))}
