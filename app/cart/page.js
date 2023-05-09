c10ba4aacb04b45a9091c592e5cc50e9a12d60db
front-chan <chan.jor.zz@gmail.com>
2023-05-09 15:10:45 +0900

    useState / onClick next.js study

--- app/cart/page.js
+++ app/cart/page.js
@@ -4,22 +4,13 @@ export default function Cart() {
     return (
         <div>
             <h4 className="title">장바구니</h4>
-            <CartItem item={carts[0]} />
-            <CartItem item={carts[1]} />
-            <Banner content="롯데카드" />
-            <Banner content="삼성카드" />
-            <Btn color="blue" />
+            {carts.map((c, i) => (
+                <CartItem item={c} />
+            ))}
         </div>
     );
 }
 
-function Banner(props) {
-    return <h5 style={{ textAlign: "center" }}>{props.content} 결제 행사중</h5>;
-}
-
-function Btn(props) {
-    return <button style={{ background: props.color }}>버튼이다</button>;
-}
 function CartItem(props) {
     return (
         <div className="cart-item">
--- app/list/page.js
+++ app/list/page.js
@@ -1,5 +1,11 @@
+"use client";
+
+import { useState } from "react";
+
 export default function List() {
     let products = ["Tomatoes", "Pasta", "Coconut"];
+    // let count = 0;
+    let [count, setCount] = useState(0);
 
     return (
         <div>
@@ -8,6 +14,23 @@ export default function List() {
                 <div className="food" key={i}>
                     <img src={`/food${i}.png`} alt={p} className="food-img" />
                     <h4>{p} $40</h4>
+                    <span> {count} </span>
+                    <button
+                        onClick={() => {
+                            // console.log(1);
+                            setCount(count + 1);
+                        }}
+                    >
+                        +
+                    </button>
+                    <button
+                        onClick={() => {
+                            // console.log(1);
+                            setCount(count - 1);
+                        }}
+                    >
+                        -
+                    </button>
                 </div>
             ))}
         </div>
